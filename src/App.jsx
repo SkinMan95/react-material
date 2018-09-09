b481a5fdc92a03c5aa33ba17350b8072bfcc19d5
Alejandro Anzola <anzola95avila@gmail.com>
2018-09-08 20:24:01 -0500

    parte 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {TodoList} from "./TodoList";
+import { TodoList } from "./TodoList";
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import moment from "moment";
+import { Login } from './login/Login';
 
 class App extends Component {
 
@@ -22,54 +23,54 @@ class App extends Component {
 
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h1 className="App-title">TODO React App</h1>
-                </header>
+              <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <h1 className="App-title">TODO React App</h1>
+              </header>
+
+              <br/>
+              <br/>
+              <form onSubmit={this.handleSubmit} className="todo-form">
+                <h3>New TODO</h3>
+                <label htmlFor="text" className="right-margin">
+                  Text:
+                </label>
+
+                <input
+                  id="text"
+                  onChange={this.handleTextChange}
+                  value={this.state.text}>
+                </input>
 
                 <br/>
                 <br/>
-                <form onSubmit={this.handleSubmit} className="todo-form">
-                    <h3>New TODO</h3>
-                    <label htmlFor="text" className="right-margin">
-                        Text:
-                    </label>
+                <label htmlFor="priority" className="right-margin">
+                  Priority:
+                </label>
 
-                    <input
-                        id="text"
-                        onChange={this.handleTextChange}
-                        value={this.state.text}>
-                    </input>
-
-                    <br/>
-                    <br/>
-                    <label htmlFor="priority" className="right-margin">
-                        Priority:
-                    </label>
-
-                    <input
-                        id="priority"
-                        type="number"
-                        onChange={this.handlePriorityChange}
-                        value={this.state.priority}>
-                    </input>
-                    <br/>
-                    <br/>
-
-                    <DatePicker
-                        id="due-date"
-                        selected={this.state.dueDate}
-                        placeholderText="Due date"
-                        onChange={this.handleDateChange}>
-                    </DatePicker>
-                    <br/>
-                    <button>
-                        Add #{this.state.items.length + 1}
-                    </button>
-                </form>
+                <input
+                  id="priority"
+                  type="number"
+                  onChange={this.handlePriorityChange}
+                  value={this.state.priority}>
+                </input>
                 <br/>
                 <br/>
-                <TodoList todoList={this.state.items}/>
+
+                <DatePicker
+                  id="due-date"
+                  selected={this.state.dueDate}
+                  placeholderText="Due date"
+                  onChange={this.handleDateChange}>
+                </DatePicker>
+                <br/>
+                <button>
+                  Add #{this.state.items.length + 1}
+                </button>
+              </form>
+              <br/>
+              <br/>
+              <TodoList todoList={this.state.items}/>
             </div>
         );
     }
